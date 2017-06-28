c1109dea71bf0f6241ff6b0d470d0e6adc06ce13
Riki <rick.libby.91@gmail.com>
2017-06-28 17:13:46 +0000

    refactored

--- countLetters.js
+++ countLetters.js
@@ -1,13 +1,11 @@
 function countLetters(stringInput) {
   var letterCount = {};
   stringInput = stringInput.split(' ').join('').split('');
-  // separate each character into element of array
 
-  for (char of stringInput) {
+  for (var char of stringInput) {
     if (char in letterCount) {
       letterCount[char] += 1;
     }
-
     else {
       letterCount[char] = 1;
     }
@@ -15,26 +13,3 @@ function countLetters(stringInput) {
 }
 
 countLetters("hello world how goes");
-
-// for (var in list) {
-//
-// }
-
-
-
-
-// Send string to countLetters function
-// Create empty object
-// --
-// Look at first letter
-// If letter doesn't exist in object, add it.
-// If letter already exist in object, increment count.
-// Repeat for each letter
-// -- or
-// Look at first letter
-// Add letter to object.
-// Count instances of letter
-// Remove letter from string
-// Repeat for each letter
-// --
-// Return object
--- countLettersIndex.js
+++ countLettersIndex.js
@@ -1,49 +1,17 @@
 function countLetters(stringInput) {
 
   var charsInString = {};
-  stringInput = stringInput.split(' ').join('').split('');
-  // separate each character into element of array
+  stringInput = stringInput.toLowerCase().split(' ').join('').split('');
 
-
-  var letterIndex;
-  for (char of stringInput) {
-    if (char in charsInString) {
-
-      charsInString[char]["count"] += 1;
-      charIndexList = charsInString[char]["indexList"];
-      charIndexList.push(stringInput.indexOf(char, charIndexList[charIndexList.length - 1] + 1));
-      console.log(charsInString[char]["indexList"] + " was increased.");
+  for (var i = 0; i < stringInput.length; i++) {
+    if (charsInString[stringInput[i]] === undefined) {
+      charsInString[stringInput[i]] = {};
+      charsInString[stringInput[i]]["indexList"] = [];
     }
-
-    else {
-      charsInString[char] = {};
-      charsInString[char]["count"] = 1;
-      charsInString[char]["indexList"] = [stringInput.indexOf(char)];
-      console.log(charsInString[char]["indexList"] + " was added.");
-    }
+    charsInString[stringInput[i]]["indexList"].push(i);
+    console.log(charsInString[stringInput[i]]["indexList"] + " was increased.");
   }
-  console.log(charsInString);
+  return charsInString;
 }
 
-countLetters("hello world how goes");
-
-
-
-
-
-
-// Send string to countLetters function
-// Create empty object
-// --
-// Look at first letter
-// If letter doesn't exist in object, add it.
-// If letter already exist in object, increment count.
-// Repeat for each letter
-// -- or
-// Look at first letter
-// Add letter to object.
-// Count instances of letter
-// Remove letter from string
-// Repeat for each letter
-// --
-// Return object
+console.log(countLetters("Hello world how goes"));
